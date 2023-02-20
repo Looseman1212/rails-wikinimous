3ef665d608d5e8f752f4f0a68cc85db98512a5c1
max reid <maxwilliamreid@gmail.com>
2023-02-20 18:06:51 +1100

    once again, havent been committing often enough

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # read all
+  get 'articles', to: 'articles#index'
+  # create article
+  get 'articles/new', to: 'articles#new', as: :new_article
+  post 'articles', to: 'articles#create'
+  # read one
+  get 'articles/:id', to: 'articles#show', as: :article
+  # update article
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+  # delete article
+  delete 'articles/:id', to: 'articles#destroy'
 end
